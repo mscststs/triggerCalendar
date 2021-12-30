28dad75d29ef8d292a1770a26a3e42f2871ad33b
mosaicpan <mosaicpan@tencent.com>
2021-12-30 21:02:32 +0800

    udpate:confidence

--- src/index.js
+++ src/index.js
@@ -57,6 +57,40 @@
       // return document.querySelector(`.Calendar-header-date[data-id='${this.id}']`).clientWidth
     }
 
+    /**
+     * 根据任务冲突，分为不同的group
+     * @param {Array} tasks 
+     * @param {Boolean} confidence 是否和数组中的任何一项冲突
+     */
+    _groupTask(tasks, confidence = true) {
+      function isConfidence(task, tasks){
+        return !!tasks.find(taskItem=>{
+          return taskItem.startDate < task.startDate && task.startDate < taskItem.endDate
+            || taskItem.startDate < task.endDate && task.endDate < taskItem.endDate
+        })
+      }
+
+      return tasks.reduce((p,c)=>{
+        for(let groupItem of p){
+          if(isConfidence(c, groupItem) === confidence){
+            groupItem.push(c)
+            return p
+          }
+        }
+        // 所有 group 都无法放下 c 时，创建一个新的 group 
+        p.push([c]);
+        return p;
+      }, [])
+    }
+
+    /**
+     * @description 构造当天所有任务的分组
+     * @param {Array} tasks 
+     */
+    _generageTaskGroups(tasks){
+      return this._groupTask(tasks,true).map(groupItem=>this._groupTask(groupItem,false))
+    }
+
 
     /**
      * 渲染结构
@@ -92,26 +126,35 @@
             ${
               this._getDays().map(dayItem=>{
                 return `<div class="Calendar-task-container-day">
-                  ${this.tasks.filter(taskItem=>{
+                  ${this._generageTaskGroups(this.tasks.filter(taskItem=>{
                     return this._getDateKey(taskItem.startDate) === dayItem.key;
-                  }).map((task,index)=>{
-                    let top = ((task.startDate.getHours() * 60 + task.startDate.getMinutes()) / (24 * 60)) * 24 * this.timeHeight;
-                    let height = (
-                      (task.endDate.valueOf() - task.startDate.valueOf())/ (60*1000) / (24 * 60)) * 24 * this.timeHeight;
-                    const Maxheight =  24 * this.timeHeight - top;
-                    console.log(Maxheight,height)
-                    height = Math.min(Maxheight, height);
-  
-                    return `
-                    <div class="Calendar-task-item" style="top:${top}px;height:${height}px; data-title="${task.title}">
-                      <div class="Calendar-task-item-label ${["orange","blue"][index%2]}"></div>
-                      <div class="Calendar-task-item-title">
-                        <div class="Calendar-task-item-text">
-                        ${task.title}
-                        </div>
-                      </div>
-                    </div>`
-
+                  }).sort((a,b)=>a.startDate - b.startDate).map((task,index)=>{
+                    task.index = index%3;
+                    return task;
+                  })).map(group=>{
+                    return `<div class="Calendar-task-group">${
+                      group.map(column=>{
+                        return `<div class="Calendar-task-column">${
+                          column.map((task)=>{
+                            let top = ((task.startDate.getHours() * 60 + task.startDate.getMinutes()) / (24 * 60)) * 24 * this.timeHeight;
+                            let height = (
+                              (task.endDate.valueOf() - task.startDate.valueOf())/ (60*1000) / (24 * 60)) * 24 * this.timeHeight;
+                            const Maxheight =  24 * this.timeHeight - top;
+                            console.log(Maxheight,height)
+                            height = Math.min(Maxheight, height);
+                            return `
+                            <div class="Calendar-task-item" style="top:${top}px;height:${height}px; data-title="${task.title}">
+                              <div class="Calendar-task-item-label ${["orange","blue","green"][task.index]}"></div>
+                              <div class="Calendar-task-item-title">
+                                <div class="Calendar-task-item-text">
+                                  ${task.title}
+                                </div>
+                              </div>
+                            </div>`
+                          }).join("")
+                        }</div>`
+                      }).join("")
+                    }</div>`
                   }).join("")
                 }
                 </div>`
@@ -121,6 +164,5 @@
         </div>
       </div>`
     }
-
   }
 })(this)
